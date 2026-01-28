966ac40b1edad442b8d5bc687a802a9cfe668717
Eliot Robson <eliot.robson24@gmail.com>
2026-01-27 19:36:54 -0500

    Add back auto

--- src/tldm/aliases.py
+++ src/tldm/aliases.py
@@ -1,19 +1,75 @@
 import itertools
+import sys
 from collections.abc import Callable, Iterable, Iterator
 from operator import length_hint
 from typing import Any, TypeVar
 
 from .std import tldm
 
+
+__all__ = [
+    "tenumerate",
+    "tzip",
+    "tmap",
+    "tproduct",
+    "trange",
+    "auto_tldm",
+]
+
+
 T = TypeVar("T")
 R = TypeVar("R")
+
+
+# Auto-detection of notebook/IPython environment
+def _get_auto_tldm() -> type[tldm]:
+    """
+    Automatically choose between `tldm.notebook` and `tldm.std`.
+
+    Returns
+    -------
+    type[tldm]
+        The appropriate tldm class based on the current environment.
+    """
+    try:
+        # Try to detect IPython/Jupyter notebook environment
+        get_ipython = sys.modules["IPython"].get_ipython  # type: ignore[attr-defined]
+        if "IPKernelApp" not in get_ipython().config:  # pragma: no cover
+            raise ImportError("console")
+
+        # Check if ipywidgets is available
+        from warnings import warn
+
+        from .notebook import WARN_NOIPYW
+        from .std import TldmWarning
+
+        try:
+            from .notebook import IProgress  # type: ignore[attr-defined]
+        except ImportError:
+            IProgress = None
+
+        if IProgress is None:
+            warn(WARN_NOIPYW, TldmWarning, stacklevel=2)
+            raise ImportError("ipywidgets")
+
+        # Use notebook version
+        from .notebook import tldm as notebook_tldm
+
+        return notebook_tldm  # type: ignore[return-value]
+    except Exception:
+        # Fallback to standard tldm
+        return tldm
+
+
+# Create auto alias - automatically selects notebook or standard tldm
+auto_tldm = _get_auto_tldm()
 
 
 def tenumerate(
     iterable: Iterable[T],
     start: int = 0,
     total: int | float | None = None,
-    tldm_class: type[tldm] = tldm,
+    tldm_class: type[tldm] | None = None,
     **tldm_kwargs: Any,
 ) -> Iterator[tuple[int, T]]:
     """
@@ -21,8 +77,10 @@ def tenumerate(
 
     Parameters
     ----------
-    tldm_class  : [default: tldm.std.tldm].
+    tldm_class  : [default: auto_tldm (automatically detected)].
     """
+    if tldm_class is None:
+        tldm_class = auto_tldm
     return enumerate(tldm_class(iterable, total=total, **tldm_kwargs), start)
 
 
@@ -34,10 +92,12 @@ def tzip(
 
     Parameters
     ----------
-    tldm_class  : [default: tldm.std.tldm].
+    tldm_class  : [default: auto_tldm (automatically detected)].
     """
     kwargs = tldm_kwargs.copy()
-    tldm_class = kwargs.pop("tldm_class", tldm)
+    tldm_class = kwargs.pop("tldm_class", None)
+    if tldm_class is None:
+        tldm_class = auto_tldm
     yield from zip(tldm_class(iter1, **kwargs), *iter2plus)
 
 
@@ -47,7 +107,7 @@ def tmap(function: Callable[..., R], *sequences: Iterable[Any], **tldm_kwargs: A
 
     Parameters
     ----------
-    tldm_class  : [default: tldm.std.tldm].
+    tldm_class  : [default: auto_tldm (automatically detected)].
     """
     for i in tzip(*sequences, **tldm_kwargs):
         yield function(*i)
@@ -59,11 +119,13 @@ def tproduct(*iterables: Iterable[T], **tldm_kwargs: Any) -> Iterator[tuple[T, .
 
     Parameters
     ----------
-    tldm_class  : [default: tldm.std.tldm].
+    tldm_class  : [default: auto_tldm (automatically detected)].
     """
     kwargs = tldm_kwargs.copy()
     repeat = kwargs.pop("repeat", 1)
-    tldm_class = kwargs.pop("tldm_class", tldm)
+    tldm_class = kwargs.pop("tldm_class", None)
+    if tldm_class is None:
+        tldm_class = auto_tldm
     try:
         lens = list(map(length_hint, iterables))
     except TypeError:
@@ -83,4 +145,4 @@ def tproduct(*iterables: Iterable[T], **tldm_kwargs: Any) -> Iterator[tuple[T, .
 
 def trange(*args: int, **kwargs: Any) -> tldm:
     """Shortcut for tldm(range(*args), **kwargs)."""
-    return tldm(range(*args), **kwargs)
+    return auto_tldm(range(*args), **kwargs)
--- src/tldm/notebook.py
+++ src/tldm/notebook.py
@@ -8,6 +8,7 @@ Includes a default `range` iterator printing to `stderr`.
 import re
 import sys
 from html import escape
+from typing import Any, TextIO
 from weakref import proxy
 
 # to inherit from the tldm class
@@ -20,9 +21,11 @@ try:
 
     clear_output(wait=False)  # Necessary when rerunning cells
 except ImportError:
-    IProgress = None
-    HBox = object
-    VBox = object
+    IProgress = None  # type: ignore[assignment, misc]
+    HBox = object  # type: ignore[assignment, misc]
+    VBox = object  # type: ignore[assignment, misc]
+    HTML = None  # type: ignore[assignment]
+    display = None  # type: ignore[assignment]
 
 
 WARN_NOIPYW = (
@@ -32,10 +35,10 @@ WARN_NOIPYW = (
 )
 
 
-class TldmHBox(HBox):
+class TldmHBox(HBox):  # type: ignore[misc]
     """`ipywidgets.HBox` with a pretty representation"""
 
-    def _json_(self, pretty=None):
+    def _json_(self, pretty: bool | None = None) -> dict[str, Any]:
         pbar = getattr(self, "pbar", None)
         if pbar is None:
             return {}
@@ -44,13 +47,13 @@ class TldmHBox(HBox):
             d["ascii"] = not pretty
         return d
 
-    def __repr__(self, pretty=False):
+    def __repr__(self, pretty: bool = False) -> str:
         pbar = getattr(self, "pbar", None)
         if pbar is None:
             return super().__repr__()
         return pbar.format_meter(**self._json_(pretty))
 
-    def _repr_pretty_(self, pp, *_, **__):
+    def _repr_pretty_(self, pp: Any, *_: Any, **__: Any) -> None:
         pp.text(self.__repr__(True))
 
 
@@ -59,10 +62,15 @@ class tldm_notebook(std_tldm):
     Experimental IPython/Jupyter Notebook widget using tldm!
     """
 
-    outer_container = None
+    outer_container: Any = None
 
     @staticmethod
-    def status_printer(_, total=None, desc=None, ncols=None):
+    def status_printer(
+        _: TextIO | None,
+        total: int | float | None = None,
+        desc: str | None = None,
+        ncols: int | str | None = None,
+    ) -> TldmHBox:
         """
         Manage the printing of an IPython/Jupyter Notebook progress bar widget.
         """
@@ -77,16 +85,15 @@ class tldm_notebook(std_tldm):
         if IProgress is None:  # #187 #451 #558 #872
             raise ImportError(WARN_NOIPYW)
         if total:
-            pbar = IProgress(min=0, max=total)
+            pbar = IProgress(min=0, max=total)  # type: ignore[misc]
         else:  # No total? Show info style bar with no progress tldm status
-            pbar = IProgress(min=0, max=1)
-            pbar.value = 1
+            pbar = IProgress(min=0, max=1)  # type: ignore[misc]
             pbar.bar_style = "info"
             if ncols is None:
                 pbar.layout.width = "20px"
 
-        ltext = HTML()
-        rtext = HTML()
+        ltext = HTML()  # type: ignore[misc]
+        rtext = HTML()  # type: ignore[misc]
         if desc:
             ltext.value = desc
         container = TldmHBox(children=[ltext, pbar, rtext])
@@ -108,13 +115,13 @@ class tldm_notebook(std_tldm):
 
     def display(
         self,
-        msg=None,
-        pos=None,
+        msg: str | None = None,
+        pos: int | None = None,
         # additional signals
-        close=False,
-        bar_style=None,
-        check_delay=True,
-    ):
+        close: bool = False,
+        bar_style: str | None = None,
+        check_delay: bool = True,
+    ) -> bool:
         # Note: contrary to native tldm, msg='' does NOT clear bar
         # goal is to keep all infos if error happens so user knows
         # at which iteration the loop failed.
@@ -125,7 +132,7 @@ class tldm_notebook(std_tldm):
             d["bar_format"] = (d["bar_format"] or "{l_bar}<bar/>{r_bar}").replace(
                 "{bar}", "<bar/>"
             )
-            msg = self.format_meter(**d)
+            msg = self.format_meter(**d)  # type: ignore[attr-defined]
 
         ltext, pbar, rtext = self.container.children
         pbar.max = self.total
@@ -168,20 +175,23 @@ class tldm_notebook(std_tldm):
         if check_delay and self.delay > 0 and not self.displayed:
             tldm_notebook.outer_container.children += (self.container,)
             if abs(self.pos) == 0:
-                display(tldm_notebook.outer_container)
+                display(tldm_notebook.outer_container)  # type: ignore[misc]
             self.displayed = True
 
+        return False
+
     @property
-    def colour(self):
+    def colour(self) -> str | None:
         if hasattr(self, "container"):
             return self.container.children[-2].style.bar_color
+        return None
 
     @colour.setter
-    def colour(self, bar_color):
+    def colour(self, bar_color: str | None) -> None:
         if hasattr(self, "container"):
             self.container.children[-2].style.bar_color = bar_color
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
         """
         Supports the usual `tldm.tldm` parameters as well as those listed below.
 
@@ -208,7 +218,7 @@ class tldm_notebook(std_tldm):
             return
 
         # Get bar width
-        self.ncols = "100%" if self.dynamic_ncols else kwargs.get("ncols", None)
+        self.ncols = "100%" if self.dynamic_ncols else kwargs.get("ncols", None)  # type: ignore[attr-defined]
 
         # Replace with IPython progress bar display (with correct total)
 
@@ -224,7 +234,7 @@ class tldm_notebook(std_tldm):
         if display_here and self.delay <= 0:
             tldm_notebook.outer_container.children += (self.container,)
             if abs(self.pos) == 0:
-                display(tldm_notebook.outer_container)
+                display(tldm_notebook.outer_container)  # type: ignore[misc]
             self.displayed = True
         self.disp = self.display
         self.colour = colour
@@ -245,7 +255,7 @@ class tldm_notebook(std_tldm):
         # NB: don't `finally: close()`
         # since this could be a shared bar which the user will `reset()`
 
-    def update(self, n=1):
+    def update(self, n: int | float = 1) -> None:
         try:
             return super().update(n=n)
         # NB: except ... [ as ...] breaks IPython async KeyboardInterrupt
@@ -257,7 +267,7 @@ class tldm_notebook(std_tldm):
         # NB: don't `finally: close()`
         # since this could be a shared bar which the user will `reset()`
 
-    def close(self):
+    def close(self) -> None:
         if self.disable:
             return
         super().close()
@@ -273,10 +283,10 @@ class tldm_notebook(std_tldm):
             else:
                 self.disp(close=True, check_delay=False)
 
-    def clear(self, *_, **__):
+    def clear(self, *_: Any, **__: Any) -> None:
         pass
 
-    def reset(self, total=None):
+    def reset(self, total: int | float | None = None) -> None:
         """
         Resets to 0 iterations for repeated use.
 
